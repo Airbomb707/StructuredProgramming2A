8bb292afacd35f21e394cd673492d8cd55afa2db
Airbomb707 <bomberpony@hotmail.com>
2021-05-18 14:20:48 -0500

    Firs

---
+++ UNIT1/Activity01.py
@@ -0,0 +1,14 @@
+num1 = 0
+num2 = 0
+add = 0
+multiply= 0
+
+num1 = int(input("Enter Number 1 \n > "))
+num2 = int(input("Enter Number 2 \n > "))
+
+add = num1+num2
+multiply = num1*num2
+
+print("The sum is: ", add)
+print("The multiply thingy is also: ", multiply)
+print("Now get outta my sight, before I divive you too")
