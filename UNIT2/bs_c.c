bab7e3a807c8d77e4d5d1a227b45401140fc7c81
Airbomb707 <bomberpony@hotmail.com>
2021-05-24 12:17:32 -0500

    May 24th

---
+++ UNIT2/bs_c.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (){
+    printf("Hewwo C world");
+    return 0;
+}
---
+++ UNIT2/bs_cpp.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main (){
+    std::cout<<"Hewwo"<<" world"<<" in C++";
+    return EXIT_SUCCESS;
+}
